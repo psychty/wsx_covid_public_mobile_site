955a9186588173583a9343e1b20e79d72f0a3a19
Rich Tyler <rich.tyler.2018@hotmail.com>
2021-02-09 17:22:39 +0000

    update

--- hospital_activity.R
+++ hospital_activity.R
@@ -1,13 +1,21 @@
+
+# Hospital admissions ####
+
+# Hospital provider trusts do not have geographically defined boundaries for their population nor do they have complete lists of registered patients. However, modeled estimates of the catchment populations for hospital provider trusts in England are provided by Public Health England (PHE). These experimental statistics estimates the number of people who are using each hospital trust or have the potential to do so. Individual acute trusts sometimes use varying methods to define the population they serve, such as patient flow, CCG derived or travel time based estimates. PHE published modelled estimates use the patient flow method.
+
+# Note: interpretation of the figures should take into account the fact that totals by date of death, particularly for recent prior days, are likely to be updated in future releases. For example as deaths are confirmed as testing positive for Covid-19, as more post-mortem tests are processed and data from them are validated. Any changes are made clear in the daily files.	
+
+
 library(easypackages)
 
-libraries(c("readxl", "readr", "plyr", "dplyr", "ggplot2", "png", "tidyverse", "reshape2", "scales", 'zoo', 'stats',"rgdal", 'rgeos', "tmaptools", 'sp', 'sf', 'maptools', 'leaflet', 'leaflet.extras', 'spdplyr', 'geojsonio', 'rmapshaper', 'jsonlite', 'grid', 'aweek', 'xml2', 'rvest', 'officer', 'flextable', 'viridis', 'epitools', 'ukcovid19'))
-
-options(scipen = 999)
+libraries("readxl", "readr", "plyr", "dplyr", "ggplot2", "png", "tidyverse", "reshape2", "scales", 'zoo', 'stats',"rgdal", 'rgeos', "tmaptools", 'sp', 'sf', 'maptools', 'leaflet', 'leaflet.extras', 'spdplyr', 'geojsonio', 'rmapshaper', 'jsonlite', 'grid', 'aweek', 'xml2', 'rvest', 'officer', 'flextable', 'viridis', 'epitools')
 
 capwords = function(s, strict = FALSE) {
   cap = function(s) paste(toupper(substring(s, 1, 1)),
                           {s = substring(s, 2); if(strict) tolower(s) else s},sep = "", collapse = " " )
   sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))}
+
+options(scipen = 999)
 
 ph_theme = function(){
   theme( 
@@ -33,18 +41,10 @@ ph_theme = function(){
   ) 
 }
 
-#github_repo_dir <- "~/Documents/GitHub/wsx_covid_public_mobile_site"
-github_repo_dir <- "~/GitHub/wsx_covid_public_mobile_site"
+#github_repo_dir <- "~/Documents/GitHub/wsx_covid_datapack_public"
+github_repo_dir <- '~/GitHub/wsx_covid_datapack_public'
 output_directory_x <- paste0(github_repo_dir, '/Outputs')
 
-
-
-
-# Hospital admissions ####
-
-# Hospital provider trusts do not have geographically defined boundaries for their population nor do they have complete lists of registered patients. However, modeled estimates of the catchment populations for hospital provider trusts in England are provided by Public Health England (PHE). These experimental statistics estimates the number of people who are using each hospital trust or have the potential to do so. Individual acute trusts sometimes use varying methods to define the population they serve, such as patient flow, CCG derived or travel time based estimates. PHE published modelled estimates use the patient flow method.
-
-# Note: interpretation of the figures should take into account the fact that totals by date of death, particularly for recent prior days, are likely to be updated in future releases. For example as deaths are confirmed as testing positive for Covid-19, as more post-mortem tests are processed and data from them are validated. Any changes are made clear in the daily files.					
 
 if(!file.exists(paste0(github_repo_dir, '/Source_files/etr.csv'))){
   download.file('https://files.digital.nhs.uk/assets/ods/current/etr.zip', paste0(github_repo_dir, '/etr.zip'), mode = 'wb')
@@ -52,24 +52,41 @@ if(!file.exists(paste0(github_repo_dir, '/Source_files/etr.csv'))){
   file.remove(paste0(github_repo_dir, '/etr.zip'), paste0(github_repo_dir, '/etr.pdf'))
 }
 
-etr <- read_csv(paste0(github_repo_dir, '/Source_files/etr.csv'),col_names = c('Code', 'Name', 'National_grouping', 'Health_geography', 'Address_1', 'Address_2', 'Address_3', 'Address_4', 'Address_5', 'Postcode', 'Open_date', 'Close_date', 'Null_1', 'Null_2', 'Null_3', 'Null_4', 'Null_5', 'Contact', 'Null_6', 'Null_7', 'Null_8', 'Amended_record_indicator', 'Null_9', 'GOR', 'Null_10', 'Null_11', 'Null_12')) %>%
-  select(Code, Name, National_grouping) %>%
-  mutate(Name = capwords(Name, strict = TRUE)) %>%
-  mutate(Name = gsub(' And ', ' and ', Name)) %>%
-  mutate(Name = gsub(' Of ', ' of ', Name)) %>%
-  mutate(Name = gsub(' Nhs ', ' NHS ', Name)) %>%
-  add_row( Code = '-', Name = 'England', National_grouping = '-')
-
-catchment_pop <- read_csv(paste0(github_repo_dir, '/Source_files/trust_catchment_population_estimates.csv')) %>% 
-  left_join(etr, by = 'Code')
 
 calls_hosp_webpage <- read_html('https://www.england.nhs.uk/statistics/statistical-work-areas/covid-19-hospital-activity/') %>%
   html_nodes("a") %>%
   html_attr("href")
 
-download.file(grep('Weekly-covid', calls_hosp_webpage, value = T), paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'), mode = 'wb')
+download.file(grep('Weekly-covid', calls_hosp_webpage, value = T), paste0(github_repo_dir,'/Source files/trust_admissions.xlsx'), mode = 'wb')
 
-trust_admissions_1 <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
+trust_admission_date <- read_excel( paste0(github_repo_dir,'/Source files/trust_admissions.xlsx'),
+                                    sheet = 'All beds COVID',
+                                    skip = 2, 
+                                    col_names = FALSE, 
+                                    n_max = 5) %>% 
+  rename(Item = ...1,
+         Description = ...2) %>%
+  filter(Item == 'Published:') %>% 
+  mutate(Description  = as.Date(as.numeric(Description), origin = "1899-12-30"))
+
+
+# We need to use data.gov to get cumulative admissions because the NHS digital data only starts on August 1st
+
+uk_gov_admissions <- read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=nhsTrust&metric=cumAdmissions&metric=cumAdmissionsByAge&metric=newAdmissions&format=csv')
+
+# admissions_df <- trust_admissions_1 %>% 
+#   group_by(Name) %>% 
+#   arrange(Name, Date) %>% 
+#   mutate(Rolling_7_day_admissions = rollapply(Admissions_or_new_cases_in_last_24hrs, 7, sum, align = 'right', fill = NA),
+#          Total_admissions = cumsum(Admissions_or_new_cases_in_last_24hrs)) %>% 
+#   mutate(Name = factor(Name, levels = c('Brighton and Sussex University Hospitals NHS Trust', 'Surrey and Sussex Healthcare NHS Trust', 'Sussex Community NHS Foundation Trust', 'Western Sussex Hospitals NHS Foundation Trust', 'South East', 'England'))) %>% 
+#   arrange(Name) %>% 
+#   ungroup()
+# 
+# admissions_table <- admissions_df %>% 
+#   filter(Date == max(Date))
+
+trust_admissions_1 <- read_excel( paste0(github_repo_dir,'/Source files/trust_admissions.xlsx'),
                                   sheet = 'Hosp ads & diag',
                                   skip = 13) %>% 
   filter(!is.na(Name)) %>% 
@@ -81,9 +98,10 @@ trust_admissions_1 <- read_excel( paste0(github_repo_dir,'/Source_files/trust_ad
   pivot_longer(names_to = 'Date', values_to = 'Admissions_or_new_cases_in_last_24hrs', cols = 5:ncol(.)) %>% 
   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"))  %>% 
   filter(Name %in% c('England', 'South East', 'Western Sussex Hospitals NHS Foundation Trust', 'Surrey and Sussex Healthcare NHS Trust', 'Sussex Community NHS Foundation Trust', 'Brighton and Sussex University Hospitals NHS Trust')) %>% 
-  select(!c('Type 1 Acute?', 'NHS England Region', 'Code'))
+  select(!c('Type 1 Acute?', 'NHS England Region', 'Code')) %>% 
+  mutate(Source_of_admission = 'All')
 
-trust_admissions_1_ch <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
+trust_admissions_1_ch <- read_excel(paste0(github_repo_dir,'/Source files/trust_admissions.xlsx'),
                                      sheet = 'Care home ads and diags',
                                      skip = 13) %>% 
   filter(!is.na(Name)) %>% 
@@ -92,42 +110,52 @@ trust_admissions_1_ch <- read_excel( paste0(github_repo_dir,'/Source_files/trust
   mutate(Name = gsub(' And ', ' and ', Name)) %>% 
   mutate(Name = gsub('Cic', 'CIC', Name)) %>% 
   mutate(Name = gsub('C.i.c', 'C.I.C', Name)) %>% 
-  pivot_longer(names_to = 'Date', values_to = 'Care_home_admissions_or_new_cases_in_last_24hrs', cols = 5:ncol(.)) %>% 
+  pivot_longer(names_to = 'Date', values_to = 'Admissions_or_new_cases_in_last_24hrs', cols = 5:ncol(.)) %>% 
   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"))  %>% 
   filter(Name %in% c('England', 'South East', 'Western Sussex Hospitals NHS Foundation Trust', 'Surrey and Sussex Healthcare NHS Trust', 'Sussex Community NHS Foundation Trust', 'Brighton and Sussex University Hospitals NHS Trust')) %>% 
-  select(!c('Type 1 Acute?', 'NHS England Region', 'Code'))
+  select(!c('Type 1 Acute?', 'NHS England Region', 'Code')) %>% 
+  mutate(Source_of_admission = 'Care home')
 
-# trust_admissions_2 <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
-#                                   sheet = 'New hosp cases',
-#                                   skip = 13) %>% 
-#   filter(!is.na(Name)) %>% 
-#   mutate(Name = capwords(Name, strict = TRUE)) %>% 
-#   mutate(Name = gsub('Nhs', 'NHS', Name)) %>% 
-#   mutate(Name = gsub(' And ', ' and ', Name)) %>% 
-#   mutate(Name = gsub('Cic', 'CIC', Name)) %>% 
-#   mutate(Name = gsub('C.i.c', 'C.I.C', Name)) %>% 
-#   pivot_longer(names_to = 'Date', values_to = 'Patients_admitted_for_first_time_with_covid_or_new_cases_in_last_24hrs', cols = 5:ncol(.)) %>% 
-#   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"))  %>% 
-#   filter(Name %in% c('England', 'South East', 'Western Sussex Hospitals NHS Foundation Trust', 'Surrey and Sussex Healthcare NHS Trust', 'Sussex Community NHS Foundation Trust', 'Brighton and Sussex University Hospitals NHS Trust')) %>% 
-#   select(!c('Type 1 Acute?', 'NHS England Region', 'Code'))
 
-# This is the same as the admissions definition except it only counts patients the first time they are admitted with or contract the virus in hospital. E.g. it excludes patients if they have more than one covid admission. 
 
-# trust_admissions_3 <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
-#                                   sheet = 'Hosp ads from comm',
-#                                   skip = 13) %>% 
-#   filter(!is.na(Name)) %>% 
-#   mutate(Name = capwords(Name, strict = TRUE)) %>% 
-#   mutate(Name = gsub('Nhs', 'NHS', Name)) %>% 
-#   mutate(Name = gsub(' And ', ' and ', Name)) %>% 
-#   mutate(Name = gsub('Cic', 'CIC', Name)) %>% 
-#   mutate(Name = gsub('C.i.c', 'C.I.C', Name)) %>% 
-#   pivot_longer(names_to = 'Date', values_to = 'Patients_admitted_for_first_time_with_covid_or_new_cases_in_last_24hrs_for_patients_admitted_in_last_seven_days', cols = 5:ncol(.)) %>% 
-#   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"))  %>% 
-#   filter(Name %in% c('England', 'South East', 'Western Sussex Hospitals NHS Foundation Trust', 'Surrey and Sussex Healthcare NHS Trust', 'Sussex Community NHS Foundation Trust', 'Brighton and Sussex University Hospitals NHS Trust')) %>% 
-#   select(!c('Type 1 Acute?', 'NHS England Region', 'Code'))
+admissions_source_df <- trust_admissions_1 %>% 
+  bind_rows(trust_admissions_1_ch) %>% 
+  pivot_wider(names_from = Source_of_admission, values_from = Admissions_or_new_cases_in_last_24hrs) %>% 
+  mutate(All = replace_na(All, 0),
+         `Care home` = replace_na(`Care home`, 0)) %>% 
+  mutate(`Not care home` = All - `Care home`) %>% 
+  select(!All) %>% 
+  pivot_longer(cols = c(`Not care home`, `Care home`), names_to = 'Source_of_admission',  values_to = 'Admissions_or_new_cases_in_last_24hrs') %>% 
+  mutate(Source_of_admission = factor(Source_of_admission, levels = c('Care home', 'Not care home'))) %>% 
+  group_by(Name, Source_of_admission) %>% 
+  arrange(Name, Source_of_admission, Date) %>% 
+  mutate(Rolling_7_day_admissions = rollapply(Admissions_or_new_cases_in_last_24hrs, 7, sum, align = 'right', fill = NA),
+         Total_admissions = cumsum(Admissions_or_new_cases_in_last_24hrs)) 
 
-# This is an approximation for cases where the virus was contracted prior to admission
+trust_x <- 'Western Sussex Hospitals NHS Foundation Trust'
+
+admissions_source_x_df <- admissions_source_df %>% 
+  filter(Name == trust_x)
+
+ggplot(admissions_source_x_df,
+       aes(x = Date,
+           y = Admissions_or_new_cases_in_last_24hrs)) +
+  geom_bar(stat = 'identity',
+           position = position_stack(reverse = TRUE),
+           width = .9,
+           aes(fill = Source_of_admission)) +
+  scale_x_date(date_labels = "%b %d",
+               breaks = seq.Date(max(admissions_source_df$Date) -(52*7), max(admissions_source_df$Date), by = 7),
+               expand = c(0.01,0.01)) +
+  scale_fill_manual(values = c('#ED7D31', '#2F5597')) +
+  labs(x = 'Date',
+       y = 'Number of admissions',
+       title = paste0('Number of new admissions with a positive COVID-19 test result; ',trust_x),
+       subtitle = paste0('New admissions or inpatients with a new diagnosis; data up to ', format(max(admissions_source_df$Date), '%d %B %Y'), ' as at ', format(trust_admission_date$Description, '%d %B %Y'))) +
+  ph_theme() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+
+rm(trust_admissions_1, trust_admissions_1_ch)
 
 trust_c19_patients_occupying_beds <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
                                   sheet = 'All beds COVID',
@@ -230,15 +258,7 @@ bed_used_df <- trust_c19_patients_occupying_ga_beds %>%
 #          Description = ...2) %>%
 #   mutate(Description = ifelse(Item == 'Published:', as.character(format(as.Date(as.numeric(Description), origin = "1899-12-30"), '%d-%b-%Y')), Description))
 
-trust_admission_date <- read_excel( paste0(github_repo_dir,'/Source_files/trust_admissions.xlsx'),
-                                    sheet = 'All beds COVID',
-                                    skip = 2, 
-                                    col_names = FALSE, 
-                                    n_max = 5) %>% 
-  rename(Item = ...1,
-         Description = ...2) %>%
-  filter(Item == 'Published:') %>% 
-  mutate(Description  = as.Date(as.numeric(Description), origin = "1899-12-30"))
+
 
 eng_beds <- bed_used_df %>% 
   filter(Name == 'England')
@@ -265,24 +285,23 @@ ggplot(eng_beds,
   ph_theme() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
 
-# ggplot(eng_beds,
-#        aes(x = Date,
-#            y = Beds)) +
-#   geom_bar(stat = 'identity',
-#            position = position_fill(reverse = TRUE),
-#            colour = '#ffffff',
-#            aes(fill = Bed_status)) +
-#   scale_x_date(date_labels = "%b %d",
-#                breaks = seq.Date(max(bed_used_df$Date) -(52*7), max(bed_used_df$Date), by = 7),
-#                expand = c(0.01,0.01)) +
-#   scale_fill_manual(values = c('#12263a', '#c96480', '#abcdef')) +
-#   labs(x = 'Date',
-#        y = 'Number of beds',
-#        title = paste0('Number of patients occupying adult beds in NHS hospital trusts'),
-#        subtitle = paste0('Share of all adult general and acute beds (%); data up to ', format(max(bed_used_df$Date), '%d %B %Y'), ' as at ', format(trust_admission_date$Description, '%d %B %Y')),
-#        caption = 'This measure only includes adult inpatient beds. It is estimated that adult beds comprised more than 99% of inpatient beds in NHS hospital trusts.') +
-#   ph_theme() +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+ggplot(eng_beds,
+       aes(x = Date,
+           y = Beds)) +
+  geom_bar(stat = 'identity',
+           colour = '#ffffff',
+           aes(fill = Bed_status)) +
+  scale_x_date(date_labels = "%b %d",
+               breaks = seq.Date(max(bed_used_df$Date) -(52*7), max(bed_used_df$Date), by = 7),
+               expand = c(0.01,0.01)) +
+  scale_fill_manual(values = c('#12263a', '#c96480', '#abcdef')) +
+  labs(x = 'Date',
+       y = 'Number of beds',
+       title = paste0('Number of patients occupying adult beds in NHS hospital trusts'),
+       subtitle = paste0('Share of all adult general and acute beds (%); data up to ', format(max(bed_used_df$Date), '%d %B %Y'), ' as at ', format(trust_admission_date$Description, '%d %B %Y')),
+       caption = 'This measure only includes adult inpatient beds. It is estimated that adult beds comprised more than 99% of inpatient beds in NHS hospital trusts.') +
+  ph_theme() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
   
 # rm(trust_admissions_1, trust_admissions_2, trust_admissions_3, trust_admissions_4, trust_admissions_5)
 
@@ -382,3 +401,8 @@ trust_admissions_5 %>%
   write_lines(paste0(output_directory_x,'/trust_bed_occupancy_df.json'))
 
 # capacity <- read_csv(paste0('https://api.coronavirus.data.gov.uk/v2/data?areaType=', area_level, '&metric=capacityPillarFour&metric=capacityPillarOne&metric=capacityPillarOneTwo&metric=capacityPillarThree&format=csv'))
+
+
+
+# STP/ICS vaccine counts ####
+# https://www.england.nhs.uk/statistics/statistical-work-areas/covid-19-vaccinations/
