cc2be400964d8e7e95717cc6449b36b8add2f283
Rich Tyler <rich.tyler.2018@hotmail.com>
2021-05-27 19:38:49 +0100

    May 27

--- Mobile_site_c19_summary_values.R
+++ Mobile_site_c19_summary_values.R
@@ -336,7 +336,8 @@ age_spec <- bind_rows(dflist) %>%
   ungroup() %>% 
   filter(Age != 'Unknown') %>% 
   mutate(Age = factor(Age, levels = c('0-4 years', '5-9 years', '10-14 years', '15-19 years', '20-24 years' , '25-29 years', '30-34 years', '35-39 years', '40-44 years', '45-49 years', '50-54 years' ,'55-59 years', '60-64 years' , '65-69 years', '70-74 years', '75-79 years', '80+ years'))) %>% 
-  mutate(Date = as.Date(Date))
+  mutate(Date = as.Date(Date)) %>% 
+  mutate(Name = ifelse(Name == 'South East', 'South East region', Name))
 
 # age_spec <- read_csv('https://coronavirus.data.gov.uk/downloads/demographic/cases/specimenDate_ageDemographic-unstacked.csv') %>% 
 #   filter(areaName %in% c('Adur', 'Arun', 'Chichester', 'Crawley', 'Horsham', 'Mid Sussex', 'Worthing', 'West Sussex', 'South East', 'England')) %>% 
