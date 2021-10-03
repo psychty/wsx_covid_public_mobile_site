56990b4ee3d66530fc2ebfb6c9392086b3175dc1
Rich Tyler <rich.tyler.2018@hotmail.com>
2021-10-03 19:57:06 +0100

    oct 3

--- Mobile_site_c19_summary_values.R
+++ Mobile_site_c19_summary_values.R
@@ -1,3 +1,4 @@
+
 library(easypackages)
 
 libraries(c("readxl", "readr", "plyr", "dplyr", "ggplot2", "png", "tidyverse", "reshape2", "scales", 'zoo', 'stats',"rgdal", 'rgeos', "tmaptools", 'sp', 'sf', 'maptools', 'leaflet', 'leaflet.extras', 'spdplyr', 'geojsonio', 'rmapshaper', 'jsonlite', 'grid', 'aweek', 'xml2', 'rvest', 'officer', 'flextable', 'viridis', 'epitools'))
